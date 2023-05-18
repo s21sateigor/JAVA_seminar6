4fbd8da364a8d1baaa6247bd5c131d7dd6aa9e1b
Igors Saters <s21sateigor@venta.lv>
2023-05-18 14:19:32 +0300

    New: add Grade class

---
+++ src/main/java/lv/venta/JAVA_seminar6/models/Grade.java
@@ -0,0 +1,30 @@
+package lv.venta.JAVA_seminar6.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "grade_table") //table in DB
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Grade {
+
+    @Column(name = "Idg")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Setter(value = AccessLevel.NONE)
+    private long idg;
+
+    @Column(name = "Gvalue")
+    @Min(value = 0)
+    @Max(value = 10)
+    private int gvalue;
+
+    //TODO add constructor later
+}
