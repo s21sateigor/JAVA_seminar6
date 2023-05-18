4274edafbced6ff0802e17eebd1f7ff519dfffd9
Igors Saters <s21sateigor@venta.lv>
2023-05-18 13:55:53 +0300

    Edit: Professor class JPA annotations and validation

--- src/main/java/lv/venta/JAVA_seminar6/models/Professor.java
+++ src/main/java/lv/venta/JAVA_seminar6/models/Professor.java
@@ -1,19 +1,49 @@
 package lv.venta.JAVA_seminar6.models;
 
 
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 @Table(name = "professor_table") //table in DB
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Professor {
 
     @Column(name = "Idp")
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long idp;
+    @Setter(value = AccessLevel.NONE)
 
+    private long idp;
+    //TODO add data JPA annotations (@column utt)
+    //TODO add validation annotations (@min @max @pattern @notnull utt)
+
+    @Column(name = "Name") // H2 title
+    @NotNull
+    @Size(min = 3, max = 20)
+    @Pattern(regexp = "[A-Z]{1}[a-z]", message = "Only Latin letters")
     private String name;
+
+    @Column(name = "Surname") // H2 title
+    @NotNull
+    @Size(min = 3, max = 20)
+    @Pattern(regexp = "[A-Z]{1}[a-z]", message = "Only Latin letters")
     private String surname;
-    
+
+    @NotNull
+    private Degree degree;
+
+    public Professor(String name, String surname, Degree degree){
+        this.name = name;
+        this.surname = surname;
+        this.degree = degree;
+    }
+
 }
 
