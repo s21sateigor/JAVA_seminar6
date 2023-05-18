fac7b16b2ddf7ebd6171cefd5584fe3bda22e0d4
Igors Saters <s21sateigor@venta.lv>
2023-05-18 14:04:00 +0300

    Edit: Student class complete

--- src/main/java/lv/venta/JAVA_seminar6/models/Student.java
+++ src/main/java/lv/venta/JAVA_seminar6/models/Student.java
@@ -2,6 +2,9 @@ package lv.venta.JAVA_seminar6.models;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +16,23 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Student {
-    @Column(name = "Ids")
+    @Column(name = "Idst")
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Setter(value = AccessLevel.NONE)
+    private long idst;
 
-    private long ids;
+    @Column(name = "Name") // H2 title
+    @NotNull
+    @Size(min = 3, max = 20)
+    @Pattern(regexp = "[A-Z]{1}[a-z]+", message = "Only Latin letters")
+    private String name;
+
+    @Column(name = "Surname") // H2 title
+    @NotNull
+    @Size(min = 3, max = 20)
+    @Pattern(regexp = "[A-Z]{1}[a-z]+", message = "Only Latin letters")
+    private String surname;
+
+
 }
