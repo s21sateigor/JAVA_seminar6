2e48f87371666a2c87d5e2ad3839498e79a26b26
Igors Saters <s21sateigor@venta.lv>
2023-05-18 14:14:57 +0300

    New: add Course class

---
+++ src/main/java/lv/venta/JAVA_seminar6/models/Course.java
@@ -0,0 +1,39 @@
+package lv.venta.JAVA_seminar6.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "course_table") //table in DB
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Course {
+
+    @Column(name = "Idc")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Setter(value = AccessLevel.NONE)
+    private long idc;
+
+    @Column(name = "Title") // H2 title
+    @NotNull
+    @Size(min = 3, max = 30)
+    @Pattern(regexp = "[A-Z]{1}[a-z\\ ]+", message = "Only Latin letters")
+    private String title;
+
+    @Column(name = "CreditPoints")
+    @Min(value = 1)
+    @Max(value = 20)
+    private int creditPoints;
+
+    //TODO add constructor later
+
+    private Professor professor;
+
+
+}
