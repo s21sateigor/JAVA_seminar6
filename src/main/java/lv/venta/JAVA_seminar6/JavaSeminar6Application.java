bb057de629b89ad4caace73f9dcdf6556715aac0
Igors Saters <s21sateigor@venta.lv>
2023-05-18 13:19:57 +0300

    New: Created project

---
+++ src/main/java/lv/venta/JAVA_seminar6/JavaSeminar6Application.java
@@ -0,0 +1,13 @@
+package lv.venta.JAVA_seminar6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSeminar6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSeminar6Application.class, args);
+	}
+
+}
---
+++ src/test/java/lv/venta/JAVA_seminar6/JavaSeminar6ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.venta.JAVA_seminar6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSeminar6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
